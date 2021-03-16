a66a0e75855bc9947046d8b1f9e427229233692d
Chris Lahaye <mail@chrislahaye.com>
2021-03-16 08:16:30 +0100

    helpers: db: refactor
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- src/app/helpers/db.ts
+++ src/app/helpers/db.ts
@@ -3,13 +3,15 @@ import mongoose from 'mongoose';
 import { databaseDebug, databaseUrl } from '../../config';
 
 mongoose.set('debug', databaseDebug);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
 
-export const connect = async (options?: mongoose.ConnectionOptions) => {
-  return await mongoose.connect(databaseUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    ...options,
-  });
+export const connect = async (
+  options?: mongoose.ConnectionOptions,
+) => {
+  return await mongoose.connect(databaseUrl, options);
 };
 
 export const disconnect = async () => {
