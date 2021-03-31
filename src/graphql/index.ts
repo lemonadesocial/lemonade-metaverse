7a41b6bf2ca31069d877e53283610acc51a11ab5
Chris Lahaye <mail@chrislahaye.com>
2021-03-31 15:08:28 +0200

    add error formatter to map app to graphql error
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,6 +1,8 @@
-import { ApolloServer } from 'apollo-server-koa';
+import { ApolloServer, ApolloError, AuthenticationError, ForbiddenError, UserInputError } from 'apollo-server-koa';
 
 import * as schema from './schema';
+
+import { AppError } from '../app/types/errors';
 
 import { apolloDebug, apolloIntropection } from '../config';
 
@@ -8,6 +10,24 @@ export const createServer = async () => {
   return new ApolloServer({
     context: ({ ctx }) => ({ app: ctx }),
     debug: apolloDebug,
+    formatError: (error) => {
+      const { originalError } = error;
+
+      if (originalError instanceof AppError) {
+        switch (originalError.status) {
+          case 401:
+            return new AuthenticationError(originalError.message);
+          case 403:
+            return new ForbiddenError(originalError.message);
+          case 422:
+            return new UserInputError(originalError.message);
+          default:
+            return new ApolloError(originalError.message, originalError.code);
+        }
+      }
+
+      return error;
+    },
     introspection: apolloIntropection,
     schema: await schema.build(),
   });
