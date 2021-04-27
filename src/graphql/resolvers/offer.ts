42a4b8161f3db8a480732e8eb67dbc578cb04bda
Chris Lahaye <mail@chrislahaye.com>
2021-04-27 11:38:54 +0200

    resolvers: offer: add id_in argument to getOffers
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- src/graphql/resolvers/offer.ts
+++ src/graphql/resolvers/offer.ts
@@ -13,11 +13,14 @@ class GetOffersResponse extends PaginatedResponse(Offer) { }
 export class OfferResolver {
   @Query(() => GetOffersResponse)
   async getOffers(
+    @Arg('id_in', () => [String], { nullable: true }) id_in: string[] | null,
     @Arg('skip', () => Int) skip: number,
     @Arg('limit', () => Int) limit: number,
     @Fields() fields: FieldsMap,
   ): Promise<GetOffersResponse> {
     const query: MongooseFilterQuery<Offer> = { };
+
+    if (id_in) query.id = { $in: id_in };
 
     const [items, total] = await Promise.all([
       fields.items ? OfferModel.find(query).skip(skip).limit(limit) : null,
