b70d4db74250dfe8cd790af86c2b18ec4f2babbf
Chris Lahaye <mail@chrislahaye.com>
2021-05-08 19:13:46 +0200

    graphql: add auto-generated where filters
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

---
+++ src/graphql/types/where-input.ts
@@ -0,0 +1,36 @@
+import { ClassType, getMetadataStorage, InputType } from 'type-graphql';
+
+export type Where<T> = { [K in keyof T as `${K & string}_eq`]?: T[K] | null; }
+  & { [K in keyof T as `${K & string}_in`]?: T[K][] | null; };
+
+export const WhereInput = <TClassType extends ClassType>(
+  BaseClass: TClassType,
+) => {
+  const metadata = getMetadataStorage();
+
+  @InputType({ isAbstract: true })
+  class WhereInputClass { }
+
+  metadata.fields.forEach((f) => {
+    if (f.target !== BaseClass) return;
+
+    metadata.fields.push(
+      {
+        ...f,
+        name: `${f.name}_eq`,
+        schemaName: `${f.name}_eq`,
+        typeOptions: { nullable: true },
+        target: WhereInputClass,
+      },
+      {
+        ...f,
+        name: `${f.name}_in`,
+        schemaName: `${f.name}_in`,
+        typeOptions: { array: true, arrayDepth: 1, nullable: true },
+        target: WhereInputClass,
+      },
+    );
+  });
+
+  return WhereInputClass as ClassType<Where<InstanceType<TClassType>>>;
+};
---
+++ src/graphql/utils/where.ts
@@ -0,0 +1,44 @@
+/* eslint-disable no-cond-assign */
+import { MongooseFilterQuery } from 'mongoose';
+
+import { Where } from '../types/where-input';
+
+const beforeEnd = (
+  str: string,
+  end: string,
+) => {
+  const n = end.length;
+
+  return str.substr(-n) === end ? str.substr(0, str.length - n) : null;
+};
+
+export const getQuery = <T>(
+  where: Where<T>,
+) => {
+  const query: Record<string, unknown> = {};
+
+  Object.entries(where).forEach(([key, value]) => {
+    let prop;
+    if (prop = beforeEnd(key, '_eq')) {
+      query[prop] = value;
+    } else if (value instanceof Array && (prop = beforeEnd(key, '_in'))) {
+      query[prop] = { $in: value };
+    }
+  });
+
+  return query as MongooseFilterQuery<T>;
+};
+
+export const validate = <T>(
+  where: Where<T>,
+  doc: Record<string, unknown>,
+) => {
+  return Object.entries(where).every(([key, value]) => {
+    let prop;
+    if (prop = beforeEnd(key, '_eq')) {
+      return doc[prop] === value;
+    } else if (value instanceof Array && (prop = beforeEnd(key, '_in'))) {
+      return value.includes(doc[prop]);
+    }
+  });
+};
