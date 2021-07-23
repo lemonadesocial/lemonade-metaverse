9ed970cf90be018e1a63db108827b4484f8c29b3
Chris Lahaye <mail@chrislahaye.com>
2021-07-23 15:22:45 +0200

    feat(helpers): add redis retry strategy to avoid reconnect spam
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- src/app/helpers/redis.ts
+++ src/app/helpers/redis.ts
@@ -4,20 +4,22 @@ import { logger } from './pino';
 
 import { redisUri } from '../../config';
 
-export const redis = new Redis(redisUri);
+const retryStrategy = () => 1000;
 
+export const redis = new Redis(redisUri);
+redis.options.retryStrategy = retryStrategy;
 redis.on('error', function onError(err: Error) {
   logger.error(err);
 });
 
 export const publisher = new Redis(redisUri);
-
+publisher.options.retryStrategy = retryStrategy;
 publisher.on('error', function onError(err: Error) {
   logger.error(err);
 });
 
 export const subscriber = new Redis(redisUri);
-
+subscriber.options.retryStrategy = retryStrategy;
 subscriber.on('error', function onError(err: Error) {
   logger.error(err);
 });
