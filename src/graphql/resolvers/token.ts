f81acf456c36ecdb36fbd165cfb76f8a51fcedeb
Chris Lahaye <mail@chrislahaye.com>
2021-08-08 15:27:15 +0200

    feat(token): add tokens resolver
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

---
+++ src/graphql/resolvers/token.ts
@@ -0,0 +1,34 @@
+import { Arg, Args, Resolver, Info, Query } from 'type-graphql';
+import { GraphQLResolveInfo } from 'graphql';
+
+import { PaginatedResponseArgs } from '../types/paginated-response';
+import { TokenModel } from '../../app/models/token';
+import { TokensResponse, TokenWhere } from '../types/token';
+
+import { getFieldTree, getFieldProjection } from '../utils/field';
+import { getFilter } from '../utils/where';
+
+@Resolver()
+class _TokensQueryResolver {
+  @Query(() => TokensResponse)
+  async tokens(
+    @Info() info: GraphQLResolveInfo,
+    @Args() { skip, limit }: PaginatedResponseArgs,
+    @Arg('where', () => TokenWhere, { nullable: true }) where?: TokenWhere | null,
+  ): Promise<TokensResponse> {
+    const fields = getFieldTree(info);
+    const query = where ? getFilter(where) : {};
+
+    const [items, total] = await Promise.all([
+      fields.items && TokenModel.aggregate([
+        { $match: query },
+        { $skip: skip },
+        { $limit: limit },
+        { $project: getFieldProjection(fields.items) },
+      ]),
+      fields.total && TokenModel.countDocuments(query),
+    ]);
+
+    return { items, total };
+  }
+}
