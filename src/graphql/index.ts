debb20412140c16137455f06928f0addbe73d347
Chris Lahaye <mail@chrislahaye.com>
2021-08-09 13:43:54 +0200

    feat: enable GraphQL playground
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,11 +1,12 @@
 import { ApolloServer } from 'apollo-server-koa';
+import { ApolloServerPluginLandingPageGraphQLPlayground, ApolloServerPluginLandingPageDisabled } from 'apollo-server-core';
 import { execute, subscribe } from 'graphql';
 import { SubscriptionServer } from 'subscriptions-transport-ws';
 import * as http from 'http';
 
 import { build as buildSchema } from './schema';
 
-import { apolloDebug, apolloIntrospection } from '../config';
+import { apolloDebug, apolloIntrospection, isProduction } from '../config';
 
 const KEEP_ALIVE = 5000;
 
@@ -21,6 +22,9 @@ export const createServers = async (
     context: ({ ctx }) => ({ app: ctx }),
     debug: apolloDebug,
     introspection: apolloIntrospection,
+    plugins: [
+      isProduction ? ApolloServerPluginLandingPageDisabled() : ApolloServerPluginLandingPageGraphQLPlayground(),
+    ],
     schema,
   });
 
