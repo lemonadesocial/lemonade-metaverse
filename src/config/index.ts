2f6da4d18c2db50f93fad9e07ad5a0046edc6f0c
Chris Lahaye <mail@chrislahaye.com>
2021-10-06 09:58:41 +0200

    feat(config): use env-var for parsing
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,22 +1,20 @@
-const parseBoolean = (value?: string) => {
-  return value === 'true' || value === '1';
-};
+import * as env from 'env-var';
 
-export const apolloDebug = parseBoolean(process.env.APOLLO_DEBUG);
-export const apolloIntrospection = parseBoolean(process.env.APOLLO_INTROSPECTION);
-export const appKey = process.env.APP_KEY || 'dummy';
-export const appPort = parseInt(process.env.APP_PORT || '4000');
-export const databaseDebug = parseBoolean(process.env.DATABASE_DEBUG);
-export const databaseUri = process.env.DATABASE_URI || 'mongodb://localhost';
-export const environment = process.env.NODE_ENV || 'development';
-export const indexerHttpUri = process.env.INDEXER_HTTP_URI || 'dummy';
-export const ipfsGatewayUri = process.env.IPFS_GATEWAY_URI || 'https://cf-ipfs.com';
-export const metricsPort = parseInt(process.env.METRICS_PORT || '9100');
-export const metricsSecret = process.env.METRICS_SECRET;
-export const redisUri = process.env.REDIS_URI;
-export const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL;
-export const sourceVersion = process.env.SOURCE_VERSION || 'unknown';
-export const web3Uri = process.env.WEB3_URI || 'dummy';
+export const apolloDebug = env.get('APOLLO_DEBUG').asBool();
+export const apolloIntrospection = env.get('APOLLO_INTROSPECTION').asBool();
+export const appKey = env.get('APP_KEY').default('dummy').asString();
+export const appPort = env.get('APP_PORT').default(4000).asIntPositive();
+export const databaseDebug = env.get('DATABASE_DEBUG').asBool();
+export const databaseUri = env.get('DATABASE_URI').default('mongodb://localhost').asString();
+export const environment = env.get('NODE_ENV').default('development').asString();
+export const indexerHttpUri = env.get('INDEXER_HTTP_URI').default('dummy').asString();
+export const ipfsGatewayUri = env.get('IPFS_GATEWAY_URI').default('https://cf-ipfs.com').asString();
+export const metricsPort = env.get('METRICS_PORT').default(9100).asIntPositive();
+export const metricsSecret = env.get('METRICS_SECRET').asString();
+export const redisUri = env.get('REDIS_URI').asString();
+export const slackWebhookUrl = env.get('SLACK_WEBHOOK_URL').asString();
+export const sourceVersion = env.get('SOURCE_VERSION').asString();
+export const web3Uri = env.get('WEB3_URI').default('dummy').asString();
 
 export const isDevelopment = environment === 'development';
 export const isProduction = environment === 'production';
