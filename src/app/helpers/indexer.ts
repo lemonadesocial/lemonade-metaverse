2ab2acbdde18a7a32faeef73dd362c0aca053647
Chris Lahaye <mail@chrislahaye.com>
2021-11-04 10:05:08 +0100

    chore(deps): upgrade dependencies
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- src/app/helpers/indexer.ts
+++ src/app/helpers/indexer.ts
@@ -1,6 +1,4 @@
-import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client/core';
 import fetch from 'node-fetch';
 
 import { indexerUrl } from '../../config';
