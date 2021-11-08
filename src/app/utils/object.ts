cc45d254acb575f0535aaee3e4e533ade3eae8f2
Chris Lahaye <mail@chrislahaye.com>
2021-11-08 18:25:41 +0100

    refactor(utils): improve exclude null type
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- src/app/utils/object.ts
+++ src/app/utils/object.ts
@@ -1,6 +1,6 @@
-export type ExcludeNull<T> = { [P in keyof T]: NonNullable<ExcludeNull<T[P]>> };
+export type ExcludeNull<T> = T extends Record<string, unknown> ? { [P in keyof T]: NonNullable<ExcludeNull<T[P]>> } : T;
 
-export const excludeNull = <T>(obj: T): ExcludeNull<T> => {
+export function excludeNull<T>(obj: T): ExcludeNull<T> {
   return Object.entries(obj).reduce((acc, [key, value]) => ({
     ...acc,
     ...value !== null ? { [key]: Object.prototype.toString.call(value) === '[object Object]' ? excludeNull(value) : value } : {},
