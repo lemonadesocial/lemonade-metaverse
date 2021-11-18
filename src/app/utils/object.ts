f461684a7f34ac2bf737a73c78fe855616052143
Chris Lahaye <mail@chrislahaye.com>
2021-11-18 14:23:58 +0100

    feat(utils): add array support to exclude null utility
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- src/app/utils/object.ts
+++ src/app/utils/object.ts
@@ -1,8 +1,8 @@
-export type ExcludeNull<T> = T extends Record<string, unknown> ? { [P in keyof T]: NonNullable<ExcludeNull<T[P]>> } : T;
+export type ExcludeNull<T> = T extends (infer P)[] ? ExcludeNull<P>[] : T extends Record<string, unknown> ? { [P in keyof T]: NonNullable<ExcludeNull<T[P]>> } : T;
 
-export function excludeNull<T>(obj: T): ExcludeNull<T> {
-  return Object.entries(obj).reduce((acc, [key, value]) => ({
+export function excludeNull<T>(val: T): ExcludeNull<T> {
+  return Object.entries(val).reduce((acc, [key, value]) => ({
     ...acc,
-    ...value !== null ? { [key]: Object.prototype.toString.call(value) === '[object Object]' ? excludeNull(value) : value } : {},
+    ...value !== null ? { [key]: value instanceof Array ? value.map((e) => excludeNull(e)) : Object.prototype.toString.call(value) === '[object Object]' ? excludeNull(value) : value } : {},
   }), {} as ExcludeNull<T>);
 }
