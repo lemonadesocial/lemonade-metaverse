e45e0b57e7094fa7e78ad38df44307dc02850c92
Chris Lahaye <mail@chrislahaye.com>
2021-11-22 14:28:21 +0100

    feat(web3): add web3 helper and contracts
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

---
+++ src/app/helpers/web3.ts
@@ -0,0 +1,20 @@
+import * as ethers from 'ethers';
+
+import * as IERC721Metadata from '../../assets/contracts/IERC721Metadata.json';
+import * as IERC721Royalty from '../../assets/contracts/IERC721Royalty.json';
+
+import { rpcUrl } from '../../config';
+
+const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+
+export const erc721MetadataContract = new ethers.Contract(
+  ethers.constants.AddressZero,
+  new ethers.utils.Interface(IERC721Metadata.abi),
+  provider
+);
+
+export const erc721RoyaltyContract = new ethers.Contract(
+  ethers.constants.AddressZero,
+  new ethers.utils.Interface(IERC721Royalty.abi),
+  provider
+);
--- src/config/index.ts
+++ src/config/index.ts
@@ -11,9 +11,9 @@ export const ipfsGatewayUrl = env.get('IPFS_GATEWAY_URL').default('https://ipfs.
 export const metricsPort = env.get('METRICS_PORT').default(9100).asIntPositive();
 export const metricsSecret = env.get('METRICS_SECRET').asString();
 export const redisUrl = env.get('REDIS_URL').asUrlString();
+export const rpcUrl = env.get('RPC_URL').default('dummy').asString();
 export const slackWebhookUrl = env.get('SLACK_WEBHOOK_URL').asUrlString();
 export const sourceVersion = env.get('SOURCE_VERSION').asString();
-export const web3Uri = env.get('WEB3_URI').default('dummy').asString();
 
 export const isDevelopment = environment === 'development';
 export const isProduction = environment === 'production';
