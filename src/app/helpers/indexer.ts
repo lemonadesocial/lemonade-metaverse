92d7f4e3ac567ecd493ff4dd031d4b7ec9fd7dab
Chris Lahaye <mail@chrislahaye.com>
2021-11-22 18:47:49 +0100

    feat(indexer): retry failed requests at backup indexer
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- src/app/helpers/indexer.ts
+++ src/app/helpers/indexer.ts
@@ -1,21 +1,55 @@
-import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client/core';
+import { ApolloClient, ApolloLink, from, HttpLink, InMemoryCache } from '@apollo/client/core';
+import { RetryLink } from '@apollo/client/link/retry';
 import * as https from 'https';
 import fetch from 'node-fetch';
 
-import { indexerUrl } from '../../config';
+import { indexerBackupUrl, indexerUrl } from '../../config';
+
+const fetchOptions = {
+  agent: new https.Agent({ keepAlive: true }),
+  timeout: 10000,
+};
+
+const httpLink = new HttpLink({
+  fetch,
+  fetchOptions,
+  uri: indexerUrl,
+});
+
+const httpLinkBackup = indexerBackupUrl ? new HttpLink({
+  fetch,
+  fetchOptions,
+  uri: indexerBackupUrl,
+}) : null;
+
+const retryLink = new RetryLink({
+  attempts: (count, operation) => {
+    if (httpLinkBackup && count === 1) {
+      operation.setContext({ useBackup: true });
+      return true;
+    }
+
+    return false;
+  },
+  delay: () => 0,
+});
+
+const terminatingLink = new ApolloLink((operation) => {
+  if (httpLinkBackup && operation.getContext().useBackup) {
+    return httpLinkBackup.request(operation);
+  }
+
+  return httpLink.request(operation);
+});
 
 export const client = new ApolloClient({
   cache: new InMemoryCache(),
   defaultOptions: {
     query: { fetchPolicy: 'no-cache' },
   },
-  link: new HttpLink({
-    fetch,
-    fetchOptions: { agent: new https.Agent({ keepAlive: true }) },
-    uri: indexerUrl,
-  }),
+  link: from([retryLink, terminatingLink]),
 });
 
-export const stop = (): void => {
+export function stop(): void {
   client.stop();
-};
+}
