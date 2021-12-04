ccf8176d3d39d1a722857db46f2f359b6be353f6
Chris Lahaye <mail@chrislahaye.com>
2021-12-04 10:49:07 +0100

    fix(web3): fix rpc requests not resolving due to ws failures
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- src/app/helpers/web3.ts
+++ src/app/helpers/web3.ts
@@ -1,4 +1,5 @@
 import * as ethers from 'ethers';
+import Web3ProviderWS from 'web3-providers-ws'
 
 import * as IERC165 from '../../assets/contracts/IERC165.json';
 import * as IERC2981 from '../../assets/contracts/IERC2981.json';
@@ -12,7 +13,20 @@ export const ERC721Metadata_INTERFACE_ID = '0x5b5e139f';
 export const ERC2981_INTERFACE_ID = '0x2a55205a';
 export const RaribleRoyaltiesV2_INTERFACE_ID = '0xcad96cca';
 
-export const provider = new ethers.providers.WebSocketProvider(rpcUrl);
+export const provider = new ethers.providers.Web3Provider(
+  // @ts-expect-error Argument of type 'WebsocketProviderBase' is not assignable to parameter of type 'ExternalProvider | JsonRpcFetchFunc'.
+  new Web3ProviderWS(rpcUrl, {
+    clientConfig: {
+      keepalive: true,
+      keepaliveInterval: 60000,
+    },
+    reconnect: {
+      auto: true,
+      delay: 1000,
+    },
+    timeout: 15000,
+  }),
+);
 
 export const erc165Contract = new ethers.Contract(
   ethers.constants.AddressZero,
---
+++ src/index.d.ts
@@ -0,0 +1,5 @@
+import { WebsocketProviderBase } from 'web3-core-helpers';
+
+declare module 'web3-providers-ws' {
+  export default WebsocketProviderBase;
+}
