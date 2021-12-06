f6532a922f95a91ab9c2224d3a3b394388fc7e3f
Chris Lahaye <mail@chrislahaye.com>
2021-12-06 16:12:05 +0100

    fix(utils): fix non-objects being recursed on when excluding null
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- src/app/utils/object.ts
+++ src/app/utils/object.ts
@@ -1,6 +1,8 @@
 export type ExcludeNull<T> = T extends (infer P)[] ? ExcludeNull<P>[] : T extends Record<string, unknown> ? { [P in keyof T]: NonNullable<ExcludeNull<T[P]>> } : T;
 
 export function excludeNull<T>(val: T): ExcludeNull<T> {
+  if (typeof val !== 'object') return val as ExcludeNull<T>;
+
   return Object.entries(val).reduce((acc, [key, value]) => ({
     ...acc,
     ...value !== null ? { [key]: value instanceof Array ? value.map((e) => excludeNull(e)) : Object.prototype.toString.call(value) === '[object Object]' ? excludeNull(value) : value } : {},
