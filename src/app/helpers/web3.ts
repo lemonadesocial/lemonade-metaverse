c29bb538948ea3d2184a6685e70aa5a3cd0bae1c
Chris Lahaye <mail@chrislahaye.com>
2021-12-21 11:46:00 +0100

    feat(web3): add number of websocket listeners metric to monitor potential leak
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- src/app/helpers/web3.ts
+++ src/app/helpers/web3.ts
@@ -1,4 +1,5 @@
 import * as ethers from 'ethers';
+import * as prom from 'prom-client';
 import Web3ProviderWS from 'web3-providers-ws'
 
 import * as IERC165 from '../../assets/contracts/IERC165.json';
@@ -13,19 +14,31 @@ export const ERC721Metadata_INTERFACE_ID = '0x5b5e139f';
 export const ERC2981_INTERFACE_ID = '0x2a55205a';
 export const RaribleRoyaltiesV2_INTERFACE_ID = '0xcad96cca';
 
+const web3ProviderWS = new Web3ProviderWS(rpcUrl, {
+  clientConfig: {
+    keepalive: true,
+    keepaliveInterval: 60000,
+  },
+  reconnect: {
+    auto: true,
+    delay: 100,
+  },
+  timeout: 15000,
+});
+
+new prom.Gauge({
+  name: 'metaverse_web3_websocket_listeners',
+  help: 'Number of metaverse web3 websocket listeners',
+  collect: function () {
+    const connection = web3ProviderWS.connection as { _listeners: Record<string, ((...args: unknown[]) => unknown)[]> } | undefined;
+
+    if (connection) this.set(Object.values(connection._listeners).reduce((acc, cur) => acc + cur.length, 0));
+  },
+});
+
 export const provider = new ethers.providers.Web3Provider(
   // @ts-expect-error Argument of type 'WebsocketProviderBase' is not assignable to parameter of type 'ExternalProvider | JsonRpcFetchFunc'.
-  new Web3ProviderWS(rpcUrl, {
-    clientConfig: {
-      keepalive: true,
-      keepaliveInterval: 60000,
-    },
-    reconnect: {
-      auto: true,
-      delay: 100,
-    },
-    timeout: 15000,
-  }),
+  web3ProviderWS,
 );
 
 export const erc165Contract = new ethers.Contract(
