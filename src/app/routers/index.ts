1cae5d2b3b275c4aa72069ac570a2b57932844ff
Chris Lahaye <mail@chrislahaye.com>
2021-12-24 09:42:14 +0100

    revert: Revert "feat(heapdump): add heapdump endpoint"
    This reverts commit 3e9e6385fbbc4748948c234982b222080228af73.

--- src/app/routers/index.ts
+++ src/app/routers/index.ts
@@ -1,9 +1,7 @@
 import Router from '@koa/router';
 
 import * as healthcheck from '../controllers/healthcheck';
-import * as heapdump from '../controllers/heapdump';
 
 export const router = new Router();
 
 router.get('/healthcheck', healthcheck.get);
-router.post('/heapdump', heapdump.post);
