a90228aa73bdc3cc1c34735ca017a605de9121b0
Chris Lahaye <mail@chrislahaye.com>
2022-04-14 21:50:00 +0200

    refactor(registry): separate fetch and create registry
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- src/app/services/registry.ts
+++ src/app/services/registry.ts
@@ -1,9 +1,9 @@
-import { erc165Contract, ERC2981_INTERFACE_ID, ERC721Metadata_INTERFACE_ID, ERC721_INTERFACE_ID, RaribleRoyaltiesV2_INTERFACE_ID } from '../helpers/web3';
 import { ethers } from 'ethers';
 import LRU from 'lru-cache';
 
-import { Network } from './network';
+import { erc165Contract, ERC2981_INTERFACE_ID, ERC721Metadata_INTERFACE_ID, ERC721_INTERFACE_ID, RaribleRoyaltiesV2_INTERFACE_ID } from '../helpers/web3';
 
+import { Network } from './network';
 import { Registry, RegistryModel } from '../models/registry';
 
 const lru = new LRU<string, Registry>({ max: 1000 });
@@ -16,36 +16,43 @@ async function supportsInterface(contract: ethers.Contract, interfaceId: string)
   }
 }
 
+async function createRegistry(network: Network, address: string) {
+  const contract = erc165Contract.connect(network.provider()).attach(address);
+
+  const [{ isERC721, supportsERC721Metadata }, supportsERC2981, supportsRaribleRoyaltiesV2] = await Promise.all([
+    (async () => {
+      const supportsERC721Metadata = await supportsInterface(contract, ERC721Metadata_INTERFACE_ID);
+      const supportsERC165 = supportsERC721Metadata !== null;
+
+      return {
+        isERC721: supportsERC721Metadata || (supportsERC165 && await supportsInterface(contract, ERC721_INTERFACE_ID)),
+        supportsERC721Metadata,
+      };
+    })(),
+    supportsInterface(contract, ERC2981_INTERFACE_ID),
+    supportsInterface(contract, RaribleRoyaltiesV2_INTERFACE_ID),
+  ]);
+
+  const registry = new Registry();
+
+  registry.id = address;
+  registry.isERC721 = isERC721 || false;
+
+  registry.supportsERC721Metadata = supportsERC721Metadata || false;
+  registry.supportsERC2981 = supportsERC2981 || false;
+  registry.supportsRaribleRoyaltiesV2 = supportsRaribleRoyaltiesV2 || false;
+
+  return registry;
+}
+
 export async function fetchRegistry(network: Network, address: string): Promise<Registry> {
+  const key = network.name + address;
   const query = { network: network.name, id: address };
-  const key = query.network + query.id;
 
   let registry = lru.get(key) || await RegistryModel.findOne(query).lean<Registry | null>();
 
   if (!registry) {
-    const contract = erc165Contract.connect(network.provider()).attach(address);
-
-    const [{ isERC721, supportsERC721Metadata }, supportsERC2981, supportsRaribleRoyaltiesV2] = await Promise.all([
-      (async () => {
-        const supportsERC721Metadata = await supportsInterface(contract, ERC721Metadata_INTERFACE_ID);
-        const supportsERC165 = supportsERC721Metadata !== null;
-
-        return {
-          isERC721: supportsERC721Metadata || (supportsERC165 && await supportsInterface(contract, ERC721_INTERFACE_ID)),
-          supportsERC721Metadata,
-        };
-      })(),
-      supportsInterface(contract, ERC2981_INTERFACE_ID),
-      supportsInterface(contract, RaribleRoyaltiesV2_INTERFACE_ID),
-    ]);
-
-    registry = new Registry();
-    registry.id = address;
-    registry.isERC721 = isERC721 || false;
-
-    registry.supportsERC721Metadata = supportsERC721Metadata || false;
-    registry.supportsERC2981 = supportsERC2981 || false;
-    registry.supportsRaribleRoyaltiesV2 = supportsRaribleRoyaltiesV2 || false;
+    registry = await createRegistry(network, address);
 
     lru.set(key, registry);
     await RegistryModel.updateOne(query, registry, { upsert: true });
