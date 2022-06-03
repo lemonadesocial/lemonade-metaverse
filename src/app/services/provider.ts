a6a70661aac33e1214d625c89eb054502e320e87
Chris Lahaye <mail@chrislahaye.com>
2022-06-03 21:16:20 +0200

    feat(provider): use reliable websocket implementation
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- src/app/services/provider.ts
+++ src/app/services/provider.ts
@@ -1,5 +1,7 @@
 import { ethers } from 'ethers';
 import { URL } from 'url';
+import SturdyWebSocket from 'sturdy-websocket';
+import WebSocket from 'ws';
 
 export interface Provider extends ethers.providers.BaseProvider {
   destroy?: () => Promise<void>;
@@ -13,7 +15,9 @@ export function createProvider(providerUrl: string): Provider {
       return new ethers.providers.AlchemyProvider(url.hostname, url.pathname.substring(1));
     case 'ws:':
     case 'wss:':
-      return new ethers.providers.WebSocketProvider(providerUrl);
+      return new ethers.providers.WebSocketProvider(
+        new SturdyWebSocket(providerUrl, { wsConstructor: WebSocket }) as any
+      );
     default:
       return new ethers.providers.JsonRpcProvider(providerUrl);
   }
