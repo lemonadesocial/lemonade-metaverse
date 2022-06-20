e9519b6ac1ab3881db44300ca59637236a0116fe
Chris Lahaye <mail@chrislahaye.com>
2022-06-20 16:19:50 +0530

    feat(provider): use reliable websocket implementation
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- src/app/services/provider.ts
+++ src/app/services/provider.ts
@@ -1,7 +1,8 @@
 import { ethers } from 'ethers';
 import { URL } from 'url';
-import SturdyWebSocket from 'sturdy-websocket';
-import WebSocket from 'ws';
+
+const WEBSOCKET_PING_INTERVAL = 60000;
+const WEBSOCKET_PONG_TIMEOUT = 5000;
 
 export interface Provider extends ethers.providers.BaseProvider {
   destroy?: () => Promise<void>;
@@ -15,10 +16,61 @@ export function createProvider(providerUrl: string): Provider {
       return new ethers.providers.AlchemyProvider(url.hostname, url.pathname.substring(1));
     case 'ws:':
     case 'wss:':
-      return new ethers.providers.WebSocketProvider(
-        new SturdyWebSocket(providerUrl, { wsConstructor: WebSocket }) as any
-      );
+      return new WebSocketProvider(providerUrl);
     default:
       return new ethers.providers.JsonRpcProvider(providerUrl);
+  }
+}
+
+const WebSocketProviderClass = (): new () => ethers.providers.WebSocketProvider => (class {} as never);
+
+class WebSocketProvider extends WebSocketProviderClass() {
+  private provider?: ethers.providers.WebSocketProvider;
+
+  private handler = {
+    get(target: WebSocketProvider, prop: string, receiver: unknown) {
+      return target.provider && Reflect.get(target.provider, prop, receiver);
+    },
+  };
+
+  constructor(private providerUrl: string) {
+    super();
+    this.create();
+
+    return new Proxy(this, this.handler);
+  }
+
+  private create() {
+    const events = this.provider?._events || [];
+
+    const provider = new ethers.providers.WebSocketProvider(this.providerUrl);
+    let pingInterval: NodeJS.Timeout | null = null;
+    let pongTimeout: NodeJS.Timeout | null = null;
+
+    provider._websocket.on('open', () => {
+      pingInterval = setInterval(() => {
+        provider._websocket.ping();
+
+        pongTimeout = setTimeout(() => { provider._websocket.terminate(); }, WEBSOCKET_PONG_TIMEOUT);
+      }, WEBSOCKET_PING_INTERVAL);
+
+      events.forEach((event) => {
+        provider._events.push(event);
+        provider._startEvent(event);
+      });
+    });
+
+    provider._websocket.on('pong', () => {
+      if (pongTimeout) clearTimeout(pongTimeout);
+    });
+
+    provider._websocket.on('close', (code: number) => {
+      if (pingInterval) clearInterval(pingInterval);
+      if (pongTimeout) clearTimeout(pongTimeout);
+
+      if (code !== 1000) this.create();
+    });
+
+    this.provider = provider;
   }
 }
