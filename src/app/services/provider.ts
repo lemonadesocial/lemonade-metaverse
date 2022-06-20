6d4412a9e5c096bbacaa51aedb547c1b7706e44f
Chris Lahaye <mail@chrislahaye.com>
2022-06-20 16:19:50 +0530

    feat(provider): periodically probe websocket connection
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- src/app/services/provider.ts
+++ src/app/services/provider.ts
@@ -3,6 +3,9 @@ import { URL } from 'url';
 import SturdyWebSocket from 'sturdy-websocket';
 import WebSocket from 'ws';
 
+const WEBSOCKET_PING_INTERVAL = 30000;
+const WEBSOCKET_PONG_TIMEOUT = 1000;
+
 export interface Provider extends ethers.providers.BaseProvider {
   destroy?: () => Promise<void>;
 }
@@ -15,10 +18,35 @@ export function createProvider(providerUrl: string): Provider {
       return new ethers.providers.AlchemyProvider(url.hostname, url.pathname.substring(1));
     case 'ws:':
     case 'wss:':
-      return new ethers.providers.WebSocketProvider(
-        new SturdyWebSocket(providerUrl, { wsConstructor: WebSocket }) as any
-      );
+      return createWebSocketProvider(providerUrl);
     default:
       return new ethers.providers.JsonRpcProvider(providerUrl);
   }
+}
+
+function createWebSocketProvider(url: string) {
+  const sturdyWebSocket = new SturdyWebSocket(url, { wsConstructor: WebSocket });
+  const provider = new ethers.providers.WebSocketProvider(sturdyWebSocket as never);
+
+  let pingInterval: NodeJS.Timer;
+  let pongTimeout: NodeJS.Timeout;
+
+  provider._websocket.on('open', () => {
+    pingInterval = setInterval(() => {
+      provider._websocket.ping();
+
+      pongTimeout = setTimeout(() => { sturdyWebSocket.reconnect(); }, WEBSOCKET_PONG_TIMEOUT);
+    }, WEBSOCKET_PING_INTERVAL);
+  });
+
+  provider._websocket.on('pong', () => {
+    clearInterval(pongTimeout);
+  });
+
+  provider._websocket.on('close', () => {
+    clearInterval(pingInterval);
+    clearTimeout(pongTimeout);
+  });
+
+  return provider;
 }
