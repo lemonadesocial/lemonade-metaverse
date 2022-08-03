1c2b777d918b0e5af5a7eff49266871061fa4541
Chris Lahaye <mail@chrislahaye.com>
2022-08-03 17:28:37 +0200

    refactor(enrich): refactor queue
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- src/app/services/enrich/queue.ts
+++ src/app/services/enrich/queue.ts
@@ -3,11 +3,21 @@ import * as assert from 'assert';
 
 import { JobData, ORDERS_KEY, QUEUE_NAME } from './shared';
 
-import { redis } from '../../helpers/redis';
 import { connection } from '../../helpers/bullmq';
+import { redis } from '../../helpers/redis';
 
 import { Order } from '../../models/order';
 import { Token } from '../../models/token';
+
+interface Item {
+  token: Token;
+  orders?: Order[];
+}
+interface Job {
+  name: string;
+  data: JobData;
+  opts?: BulkJobOptions;
+}
 
 let queue: Queue<JobData> | undefined;
 let queueScheduler: QueueScheduler | undefined;
@@ -17,28 +27,13 @@ export async function start() {
   queueScheduler = new QueueScheduler(QUEUE_NAME, { connection });
 }
 
-/**
- * Adds the given items to the queue.
- *
- * Each item becomes a new job, unless a job for the item's token already exists. An token's orders are stored outside the job data as they
- * need to be updated even though the job already exists. Our highest priority is avoiding multiple enriches for the same token, so we extend a token's
- * orders instead of creating a new job for each token order pair.
- *
- * We have the following cases:
- *  1) job 1 uses the updated data, when ingress updates the data before job 1 starts or reaches the query statement, and attempts to create the job before job 1 completes
- *  2) job 1 uses the updated data and job 2 uses none, when ingress updates the data before job 1 starts or reaches the query statement, and creates job 2 after job 1 completes
- *  3) job 1 uses the oudated data and job 2 uses the updated data, when ingress updates the data after the job 1 reaches the query statement, and creates job 2 after job 1 completes
- *  4) job 1 uses the outdated data, when ingress updates the data after job 1 reaches the query statement, and attempts to create the job before job 1 completes (i.e. while it is still running) (@todo: apply locking)
- */
-export async function enqueue(
-   ...items: { token: Token; orders?: Order[] }[]
-): Promise<void> {
+export async function enqueue(...items: Item[]) {
   if (!items.length) return;
 
   assert.ok(queue);
 
   const commands: string[][] = [];
-  const jobs: { name: string; data: JobData; opts?: BulkJobOptions }[] = [];
+  const jobs: Job[] = [];
 
   items.forEach(({ token, orders }) => {
     const jobId = `${token.contract}:${token.id}`;
