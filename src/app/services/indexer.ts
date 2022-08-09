54ba3e5face5863c43e3b72c95bda75a9a322ad1
Chris Lahaye <mail@chrislahaye.com>
2022-08-09 15:14:39 +0200

    feat(indexer): reuse in-memory cache instance
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- src/app/services/indexer.ts
+++ src/app/services/indexer.ts
@@ -7,17 +7,18 @@ const agent: Record<string, http.Agent> = {
   'http:': new http.Agent({ keepAlive: true }),
   'https:': new https.Agent({ keepAlive: true }),
 };
-
 const fetchOptions: RequestInit = {
   agent: (url) => agent[url.protocol],
   timeout: 10000,
 };
 
+const cache = new InMemoryCache();
+
 export type Indexer = ApolloClient<NormalizedCacheObject>;
 
 export function createIndexer(indexerUrl: string): Indexer {
   return new ApolloClient({
-    cache: new InMemoryCache(),
+    cache,
     defaultOptions: {
       query: { fetchPolicy: 'no-cache' },
     },
