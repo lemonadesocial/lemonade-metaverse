5aa23296332618ca8acdc3873c984e726046679a
Chris Lahaye <mail@chrislahaye.com>
2022-12-03 11:31:31 +0100

    refactor: fix exclude null util type error
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- src/app/utils/object.ts
+++ src/app/utils/object.ts
@@ -1,6 +1,6 @@
 export type ExcludeNull<T> = T extends (infer P)[] ? ExcludeNull<P>[] : T extends Record<string, unknown> ? { [P in keyof T]: NonNullable<ExcludeNull<T[P]>> } : T;
 
-export function excludeNull<T>(val: T): ExcludeNull<T> {
+export function excludeNull<T extends object>(val: T): ExcludeNull<T> {
   if (typeof val !== 'object') return val as ExcludeNull<T>;
 
   return Object.entries(val).reduce((acc, [key, value]) => ({
